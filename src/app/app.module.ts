3bde0ec786137debe3588af93b652245773ef704
Val <57106675+vcthuang@users.noreply.github.com>
2020-03-01 21:53:03 -0800

    update style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+// Grocery Component is the only component created for this project.
+// It allows user to add, edit, delete items from shopping list.
+// It also allows user to strike through an item to mark it as done.
 import { GroceryComponent } from './grocery/grocery.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { doesNotReject } from 'assert';
 
+// we added bootstrap style for this project
 @Component({
   selector: 'app-grocery',
   templateUrl: './grocery.component.html',
   styleUrls: [
+    './grocery.component.css',
     '../../assets/css/bootstrap.min.css',
   ]
 })
+
+
 export class GroceryComponent implements OnInit {
   
+  // Initialize task - an item on the shopping list and 
+  // tasks - the entire shopping list.
   task = {
     name: '',
     id: 0
   };
   
+  // tasks {
+  //   id: unique timestamp
+  //   name: string holds shopping item
+  //   strike: boolean tells if the task is done
+  // }
   tasks = [];
 
   constructor() { }
@@ -21,11 +33,16 @@ export class GroceryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // This function is called when the user click on the input box
   onClick(){
+    
+    // add an entry to tasks if it's a new shopping item
     if(this.task.id == 0){
       this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false});
     }
     
+    // reset task or otherwise the shopping item will be shown on the input box 
+    // which makes user input unfriendly.  It is also used to end editing an item.
     this.task = {
       name: '',
       id: 0
@@ -35,11 +52,15 @@ export class GroceryComponent implements OnInit {
     console.log(this.tasks);
   }
 
+  // This function is called when the user clicks on pencil mark to modify an entry
   onEdit(item){
     this.task = item;
   }
 
+  // This function is called when the user click on x mark to delete an entry
   onDelete(item){
+
+    // loop through entire tasks.  If id matches, the entry is deleted from tasks
     for(var i = 0;i < this.tasks.length; i++){
         if(item.id == this.tasks[i].id){
             this.tasks.splice(i,1);
@@ -48,7 +69,11 @@ export class GroceryComponent implements OnInit {
     }
   }
 
+  // This function is called when the user click on the item to mark done or undone
   onStrike(item){
+
+    // loop through entire tasks.  
+    // If id matches, strike will be set to the opposite of the current state
     for(var i = 0;i < this.tasks.length; i++){
       if(item.id == this.tasks[i].id){
         if(this.tasks[i].strike){
