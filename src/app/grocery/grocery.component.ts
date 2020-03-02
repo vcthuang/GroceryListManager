894468f9b337fcd59c93b1e402d128c14c17da90
Val <57106675+vcthuang@users.noreply.github.com>
2020-03-01 16:44:02 -0800

    Add Edit and Delete Item

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -8,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class GroceryComponent implements OnInit {
+  
+  task = {
+    name: '',
+    id: 0
+  };
+  
+  tasks = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(){
+    if(this.task.id == 0){
+      this.tasks.push({id: (new Date()).getTime(),name: this.task.name});
+    }
+    console.log(this.task);
+    console.log(this.tasks);
+  }
+
+  onEdit(item){
+    this.task = item;
+  }
+
+  onDelete(item){
+    for(var i = 0;i < this.tasks.length; i++){
+        if(item.id == this.tasks[i].id){
+            this.tasks.splice(i,1);
+            break;
+        }
+    }
+  }
 }
